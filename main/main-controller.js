85836461e2f683cf229264b57bc35ff8b96ce411
Kelyn D. Mann <kdmann@imi.local>
2017-04-28 14:02:49 -0400

    First Commit

---
+++ main/main-controller.js
@@ -0,0 +1,11 @@
+angular.module('myApp').controller('MainController', MainController);
+
+function MainController($http) {
+    var vm = this;
+
+    $http.get('http://swapi-tpiros.rhcloud.com/films').then(function(response) {
+        vm.films = response.data;
+    });
+
+    vm.name='Kelyn';
+}
